4fb3187e8f4313dba23d091957d64b8f0d5ccd0b
solusimicro <solusimicro@gmail.com>
2025-12-29 20:35:52 +0700

    Delete vibration_feature directory

--- vibration_feature/app/services/mtbf.py
+++
@@ -1,8 +0,0 @@
-def estimate_mtbf(base_mtbf_hours, health_index):
-    """
-    base_mtbf_hours: OEM / historical MTBF
-    health_index: 0–1
-    """
-    return round(base_mtbf_hours * health_index, 1)
-# MTBF estimation service
-# This service estimates the Mean Time Between Failures (MTBF) based on a base MTBF and the health index of the machinery.
